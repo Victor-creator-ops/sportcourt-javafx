45127ddb6c873671f75e02deb6a5aa718a0272e5
Victor-Creator-Ops <victor.feitosa@fatec.sp.gov.br>
2025-11-28 22:43:40 -0300

    V0.6.1 > Criado model ItemComanda

---
+++ src/main/java/br/com/sportcourt/model/ItemComanda.java
@@ -0,0 +1,41 @@
+package br.com.sportcourt.model;
+
+public class ItemComanda {
+
+    private int id;
+    private int comandaId;
+    private int produtoId;
+    private int quantidade;
+    private double valorUnit;
+    private double valorTotal;
+
+    public ItemComanda() {}
+
+    public ItemComanda(int id, int comandaId, int produtoId,
+                       int quantidade, double valorUnit, double valorTotal) {
+        this.id = id;
+        this.comandaId = comandaId;
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+        this.valorUnit = valorUnit;
+        this.valorTotal = valorTotal;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getComandaId() { return comandaId; }
+    public void setComandaId(int comandaId) { this.comandaId = comandaId; }
+
+    public int getProdutoId() { return produtoId; }
+    public void setProdutoId(int produtoId) { this.produtoId = produtoId; }
+
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+
+    public double getValorUnit() { return valorUnit; }
+    public void setValorUnit(double valorUnit) { this.valorUnit = valorUnit; }
+
+    public double getValorTotal() { return valorTotal; }
+    public void setValorTotal(double valorTotal) { this.valorTotal = valorTotal; }
+}
