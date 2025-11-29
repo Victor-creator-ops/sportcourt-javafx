bd4047aeb99be1a796f9ee860d766bf1c6a404f3
Victor-Creator-Ops <victor.feitosa@fatec.sp.gov.br>
2025-11-28 22:43:04 -0300

    V0.6.0 > Criado model Comanda

---
+++ src/main/java/br/com/sportcourt/model/Comanda.java
@@ -0,0 +1,30 @@
+package br.com.sportcourt.model;
+
+public class Comanda {
+
+    private int id;
+    private Integer reservaId; // pode ser null
+    private double total;
+    private String status; // ABERTA, FECHADA
+
+    public Comanda() {}
+
+    public Comanda(int id, Integer reservaId, double total, String status) {
+        this.id = id;
+        this.reservaId = reservaId;
+        this.total = total;
+        this.status = status;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public Integer getReservaId() { return reservaId; }
+    public void setReservaId(Integer reservaId) { this.reservaId = reservaId; }
+
+    public double getTotal() { return total; }
+    public void setTotal(double total) { this.total = total; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
